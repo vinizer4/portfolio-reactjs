7e55cc1b5f0f332e707f6846106046d163349dbd
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-01-19 00:19:48 -0300

    feat: implemented header component

---
+++ src/sections/header/data.js
@@ -0,0 +1,21 @@
+import {
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+  AiOutlineGithub,
+} from "react-icons/ai";
+
+const data = [
+  {
+    id: 1,
+    link: "https://www.instagram.com/vini_ts/",
+    icon: <AiOutlineInstagram />,
+  },
+  {
+    id: 2,
+    link: "https://www.linkedin.com/in/vinicius-teixeira-saraiva/",
+    icon: <AiOutlineLinkedin />,
+  },
+  { id: 3, link: "https://github.com/vinizer4", icon: <AiOutlineGithub /> },
+];
+
+export default data;
