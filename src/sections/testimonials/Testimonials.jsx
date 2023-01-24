c1fcc9fdd3221582d5168951e74a39beaf5bec1e
ViniciusTS <vinicius.ts.online@gmail.com>
2023-01-23 23:30:32 -0300

    faet: created testimonials page

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,35 +1,53 @@
-import "./testimonials.css";
-
-import { Swiper, SwiperSlide } from "swiper/rect";
-
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/autoplay"
 
-import "./testimonials.css";
-
-import testimonials from "./data";
+// import required modules
 import { Pagination } from "swiper";
+
+
+import testimonials from './data'
+import Testimonial from "./Testimonial";
+import './testimonials.css'
+
 
 const Testimonials = () => {
   return (
     <section id="testimonials">
       <h2>What My Clients Say</h2>
-      <p>There are unbiased testmonials from some of my clients</p>
+      <p>
+        These are unbiased testmonials from some of my clients
+      </p>
       <div className="container">
-        <Swiper
-          slidesPerView={3}
-          spaceBetween={30}
-          pagination={{
-            clickable: true,
-          }}
-          modules={{ Pagination }}
-          className="mySwiper"
-        >
-          <SwiperSlide></SwiperSlide>
-        </Swiper>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        breakpoints={{
+          601: {slidesPerView: 2},
+          1025: {slidesPerView: 3}
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination, Autoplay]}
+        autoplay={true}
+        className="mySwiper"
+      >
+        {
+          testimonials.map(testimonial => (
+            <SwiperSlide key={testimonial.id}>
+              <Testimonial testimonial={testimonial}/>
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Testimonials;
+export default Testimonials
